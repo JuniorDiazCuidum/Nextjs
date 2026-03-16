d8d2c3f08298a87515e65a8e229bdfafd97229b6
Junior <junior.diaz@cuidum.com>
2026-03-16 12:18:58 +0100

    next js

--- next-js-13-app/app/posts/page.jsx
+++ next-js-13-app/app/posts/page.jsx
@@ -1,5 +1,19 @@
-export default function PostsPage() {
+const fetchPosts = () => {
+    return fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(res => res.json())
+}
+
+export default async function PostsPage({ params }) {
+    const posts = await fetchPosts()
+
     return (
-        <h1>Aqui mostramos los posts ... </h1>
-    );
+        <section>
+            {posts.slice(0, 5).map(post => (
+                <article key={post.id}>
+                    <h2>{post.title}</h2>
+                    <p>{post.body}</p>
+                </article>
+            ))}
+        </section>
+    )
 }
